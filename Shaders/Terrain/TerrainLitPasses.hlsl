6b1ddb76814ae49021b5754d0b88b7d4a35c2a5d
智樂堂_李宇凡 <w963963555577@gmail.com>
2020-07-31 16:41:31 +0800

    Terrain TextureNoRepeat

--- Shaders/Terrain/TerrainLitPasses.hlsl
+++ Shaders/Terrain/TerrainLitPasses.hlsl
@@ -185,15 +185,45 @@
     }
     
     #ifndef TERRAIN_SPLAT_BASEPASS
-        
+        float4 hash4(float2 p)
+        {
+            return frac(sin(float4(1.0 + dot(p, float2(37.0, 17.0)),
+            2.0 + dot(p, float2(11.0, 47.0)),
+            3.0 + dot(p, float2(41.0, 29.0)),
+            4.0 + dot(p, float2(23.0, 31.0)))) * 103.0);
+        }
+        half4 TextureNoTile(Texture2D < float4 > map, in SamplerState state, in float2 uv)
+        {
+            float2 p = floor(uv);
+            float2 f = frac(uv);
+            
+            // voronoi contribution
+            float4 va = 0.0;
+            float wt = 0.0;
+            for (int j = -1; j <= 1; j ++)
+            for (int i = -1; i <= 1; i ++)
+            {
+                float2 g = float2(i, j);
+                float4 o = hash4(p + g);
+                float2 r = g - f + o.xy;
+                float d = dot(r, r);
+                float w = exp(-5.0 * d);
+                float4 c = SAMPLE_TEXTURE2D(map, state, uv + o.zw);
+                va += w * c;
+                wt += w;
+            }
+            
+            // normalization
+            return va / wt;
+        }
         void SplatmapMix(float4 uvMainAndLM, float4 uvSplat01, float4 uvSplat23, inout half4 splatControl, out half weight, out half4 mixedDiffuse, out half4 defaultSmoothness, inout half3 mixedNormal)
         {
             half4 diffAlbedo[4];
             
-            diffAlbedo[0] = SAMPLE_TEXTURE2D(_Splat0, sampler_Splat0, uvSplat01.xy);
-            diffAlbedo[1] = SAMPLE_TEXTURE2D(_Splat1, sampler_Splat0, uvSplat01.zw);
-            diffAlbedo[2] = SAMPLE_TEXTURE2D(_Splat2, sampler_Splat0, uvSplat23.xy);
-            diffAlbedo[3] = SAMPLE_TEXTURE2D(_Splat3, sampler_Splat0, uvSplat23.zw);
+            diffAlbedo[0] = TextureNoTile(_Splat0, sampler_Splat0, uvSplat01.xy);
+            diffAlbedo[1] = TextureNoTile(_Splat1, sampler_Splat0, uvSplat01.zw);
+            diffAlbedo[2] = TextureNoTile(_Splat2, sampler_Splat0, uvSplat23.xy);
+            diffAlbedo[3] = TextureNoTile(_Splat3, sampler_Splat0, uvSplat23.zw);
             
             // This might be a bit of a gamble -- the assumption here is that if the diffuseMap has no
             // alpha channel, then diffAlbedo[n].a = 1.0 (and _DiffuseHasAlphaN = 0.0)
@@ -459,7 +489,7 @@
         half bakedGIArea;
         
         InitializeInputDataFromCustomShadow(IN, normalTS, bakedGIArea, inputData);
-        half4 color = UniversalFragmentPBR(inputData, albedo, metallic, /* specular */ half3(0.0h, 0.0h, 0.0h),0.0 /*smoothness*/, occlusion, /* emission */ half3(0, 0, 0), alpha);
+        half4 color = UniversalFragmentPBR(inputData, albedo, metallic, /* specular */ half3(0.0h, 0.0h, 0.0h), 0.0 /*smoothness*/, occlusion, /* emission */ half3(0, 0, 0), alpha);
         
         SplatmapFinalColor(color, inputData.fogCoord);
         
